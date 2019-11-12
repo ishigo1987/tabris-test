0d4c3395586dc971d4b5ad06d073498ef1b1ac9a
ishigo1987 <stephnguets@yahoo.fr>
2019-11-12 20:02:57 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-const workerContacts = new Worker("./src/helpers/worker.js");
+
+      function onSuccess(contacts) {
+	       console.log('Found ' + contacts.length + ' contacts.');
+	       const workerContacts = new Worker("./src/helpers/worker.js");
                workerContacts.onmessage = (event) => {
                   workerContacts.terminate();
                };
@@ -6,4 +9,18 @@ const workerContacts = new Worker("./src/helpers/worker.js");
                  console.log(`onerror: ${JSON.stringify(error)}`);
                  workerContacts.terminate();
                };
-                workerContacts.postMessage(['697644414','0033648138928','#155#','+4917674614474','669341994']);
+                workerContacts.postMessage(contacts);
+
+      };
+      function onError(contactError) {
+      	console.log(contactError);
+      };
+
+      const options      = new ContactFindOptions();
+      options.filter   = "";
+      options.multiple = true;
+      // options.desiredFields = [navigator.contacts.fieldType.id];
+      options.hasPhoneNumber = true;
+      const fields       = [navigator.contacts.fieldType.country,navigator.contacts.fieldType.phoneNumbers,navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+      navigator.contacts.find(fields, onSuccess, onError, options);
+  
