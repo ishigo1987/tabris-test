16f097a4acf88190a0adfc01d2dda31a55026829
ishigo1987 <stephnguets@yahoo.fr>
2019-11-14 08:07:06 +0100

    Update

--- src/helpers/permissions.js
+++ src/helpers/permissions.js
@@ -1,18 +1,28 @@
 module.exports = ()=>{
   return new Promise((resolve)=>{
-  	 const permissions = cordova.plugins.permissions;
-     	   permissions.requestPermission(permissions.READ_CONTACTS, success, error);
+ //  	 const permissions = cordova.plugins.permissions;
+ //     	   permissions.requestPermission(permissions.READ_CONTACTS, success, error);
  
-	function error() {
-	  console.log('user refuse to get contact access');
-	}
+	// function error() {
+	//   console.log('user refuse to get contact access');
+	// }
  
-	function success( status ) {
-	  if(status.hasPermission === true){
-        resolve({Message:"Permission granted"});
-	  }else{
-	  	error();
-	  }
-	}
+	// function success( status ) {
+	//   if(status.hasPermission === true){
+ //        resolve({Message:"Permission granted"});
+	//   }else{
+	//   	error();
+	//   }
+	// }
+	const {permission} = require('tabris');
+	if (permission.isAuthorized('contact')) {
+  console.log('Camera permission is available');
+} else {
+    permission.requestAuthorization('camera').then((responseStatus)=>{
+    	console.log(`Camera permission has been ${responseStatus}.`);
+    })
+    
+  
+}
   });
 };
