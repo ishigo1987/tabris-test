44d0ecd9206ee66cc4acde91663a4eda4f4c2357
ishigo1987 <stephnguets@yahoo.fr>
2019-11-25 13:37:06 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-function onSuccess(contacts) {
-	console.log('Found ' + contacts.length + ' contacts.');
-};
+// function onSuccess(contacts) {
+// 	console.log('Found ' + contacts.length + ' contacts.');
+// };
 
-function onError(contactError) {
-	console.log('onError!');
-};
+// function onError(contactError) {
+// 	console.log('onError!');
+// };
 
-// find all contacts with 'Bob' in any name field
-var options      = new ContactFindOptions();
-options.filter   = "697644414";
-options.multiple = true;
-options.desiredFields = [navigator.contacts.fieldType.id];
-options.hasPhoneNumber = true;
-var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
-navigator.contacts.find(fields, onSuccess, onError, options);
+// // find all contacts with 'Bob' in any name field
+// var options      = new ContactFindOptions();
+// options.filter   = "697644414";
+// options.multiple = true;
+// options.desiredFields = [navigator.contacts.fieldType.id];
+// options.hasPhoneNumber = true;
+// var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+// navigator.contacts.find(fields, onSuccess, onError, options);
+console.log(navigator)
