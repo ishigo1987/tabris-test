1bbb87aef8cd5597e11ab6b74462dc369915d456
ishigo1987 <stephnguets@yahoo.fr>
2019-11-28 13:04:55 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,9 @@
-// function onSuccess(contacts) {
-// 	console.log('Found ' + contacts.length + ' contacts.');
-// };
+function success (countryCode) {
+  console.log(countryCode);
+}
 
-// function onError(contactError) {
-// 	console.log('onError!');
-// };
+function error () {
+  console.warn('An error occured');
+}
 
-// // find all contacts with 'Bob' in any name field
-// var options      = new ContactFindOptions();
-// options.filter   = "697644414";
-// options.multiple = true;
-// options.desiredFields = [navigator.contacts.fieldType.id];
-// options.hasPhoneNumber = true;
-// var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
-// navigator.contacts.find(fields, onSuccess, onError, options);
-console.log(navigator)
+plugins.country.get(success, error);
