30a9a78373b335eacde2b7c34b2e60cbaf130050
ishigo1987 <stephnguets@yahoo.fr>
2019-11-30 21:20:25 +0100

    Update

--- src/helpers/backgroundService.js
+++ src/helpers/backgroundService.js
@@ -1,13 +1,22 @@
 module.exports = ()=>{
-	let count = 0
-	window.BackgroundService.start(
-      t(),
-    function() { console.log('err') }
-)
-	function t(){
-	   let count = 0;
-      setInterval(()=>{
-	      console.log(`test ${count++}`)
-      },1500);
-	}
+// 	let count = 0
+// 	window.BackgroundService.start(
+//       t(),
+//     function() { console.log('err') }
+// )
+// 	function t(){
+// 	   let count = 0;
+//       setInterval(()=>{
+// 	      console.log(`test ${count++}`)
+//       },1500);
+// 	}
+let count = 0;
+cordova.plugins.jobScheduler.schedule({
+   minimumLatency: 10, // seconds
+   overrideDeadline: 10, //seconds
+   callback: function() { 
+     
+     console.log(`test ${count++}`)
+   } // what to do when the job is fired
+ });
 }
--- src/helpers/checkSms.js
+++ src/helpers/checkSms.js
@@ -1,7 +1,8 @@
 module.exports = ()=>{
-      CordovaSMS.checkDefault((result)=>{
-          console.log(JSON.parse(result).currentDefault);
-      },(err)=>{
-         console.log(err)
-      });
+      CordovaSMS.setDefault(null, null, "app.com.tidou");
+     //User will be prompt by Android system dialog to change the SMS app, the yes|no button click can be listen trough
+     CordovaSMS.onDefaultSelected((result)=>{
+         console.log(result)
+         //result value will be true if user hits Yes button, else it will result to false.
+     });
 };
