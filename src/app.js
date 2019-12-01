f47cf223c868eedb31d7ef166257b73cf8ce5083
ishigo1987 <stephnguets@yahoo.fr>
2019-12-01 16:41:12 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const{app} = require("tabris")
-cordova.plugins.foregroundService.start('GPS Running', 'Background Service', 'myicon', 3, 10);
-require("./helpers/powerManagement.js")();
-require("./helpers/wifiLock.js")();
-require("./helpers/batteryOptimisation.js")();
-require("./helpers/backgroundService.js")();
+// const{app} = require("tabris")
+// cordova.plugins.foregroundService.start('GPS Running', 'Background Service', 'myicon', 3, 10);
+// require("./helpers/powerManagement.js")();
+// require("./helpers/wifiLock.js")();
+// require("./helpers/batteryOptimisation.js")();
+// require("./helpers/backgroundService.js")();
 // require("./helpers/checkSms.js")();
