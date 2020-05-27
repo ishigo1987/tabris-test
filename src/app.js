2d70d134fa1a9f40a8ee6df6c60ea09cd13856ea
ishigo1987 <stephnguets@yahoo.fr>
2020-05-27 07:21:30 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const{app,Composite,CollectionView,TextView,contentView} = require("tabris");
+const{app,Composite,CollectionView,TextView,contentView, TextInput, Button} = require("tabris");
 app.idleTimeoutEnabled = false;
 // cordova.plugins.foregroundService.start('GPS Running', 'Background Service', 'myicon', 3, 10);
 // require("./helpers/powerManagement.js")();
@@ -143,15 +143,35 @@ app.idleTimeoutEnabled = false;
 //         console.error("Error: " + error);
 //     });
 
-function success(data){
-    console.log(data)
-}
-function error(err){
-    console.log(err)
-}
-window.plugins.DocumentContract.getContract({
-    uri: 'content://com.foo.bar/xyz',  // content URI (required)
-    columns: ['_display_name']         // projection of columns (optional)           // Array => false, Object => true (default false)
-  },
-  success,                             // success callback
-  error);
+// function success(data){
+//     console.log(data)
+// }
+// function error(err){
+//     console.log(err)
+// }
+// window.plugins.DocumentContract.getContract({
+//     uri: 'content://com.foo.bar/xyz',  // content URI (required)
+//     columns: ['_display_name']         // projection of columns (optional)           // Array => false, Object => true (default false)
+//   },
+//   success,                             // success callback
+//   error);
+// new Button({centerY:0, left:15, right:15, height:75, background:"red", text:"Click me"})
+// .onTap(()=>{
+//     new Composite({top:["prev()",20], left:10, right:10})
+//     .append(
+//         new TextInput({left: ["prev()", 0], centerY: 0, right: 5, style: 'none', floatMessage: false, message:"This is a test"})
+//     ).appendTo(contentView)
+// }).appendTo(contentView);
+    // const{Composite, TextInput, TextView, contentView} = require("tabris")
+    // const headerComposite = new Composite({ top: 0, left: 0, right: 0, height: 57}).appendTo(contentView);
+    // const headerViewTitle = new TextView({ centerY: 0, left: ['prev()', 5], right: 30, textColor: "gray", text: "Test", font: 'bold 17px' }).appendTo(headerComposite);
+    // const searchCountry = new TextView({ centerY: 0, right: 15, textColor:"gray", text:"create input"})
+    // .onTap(({ target }) => {
+    //     target.dispose()
+    //     new TextInput({ width:200, centerY: 0, right: 5, message: "This is a test", style: 'none', floatMessage: false,  background: 'transparent', focused: true}).appendTo(headerComposite);
+    // }).appendTo(headerComposite);
+
+
+    // setTimeout(()=>{
+    //     headerComposite.dispose();
+    // },15000);
