ee5f04043723c1aa10060b953f9d75c4a611395e
ishigo1987 <stephnguets@yahoo.fr>
2020-07-26 09:06:45 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 
-const{permission} = require("tabris");
+const{permission, CameraView, device, contentView} = require("tabris");
 
 if (permission.isAuthorized('camera')) {
-    console.log("Permission granted");   
+    camera()  
 } else {
     permission.requestAuthorization("camera").then((responseStatus) => {
         console.log(responseStatus)
         if (responseStatus !== "granted") {
             // Permission denied
         } else {
-            console.log("Permission granted");
+            camera()
         }
     });
+}
+
+function camera(){
+    const camera = device.cameras[0];
+    camera.active = true;
+
+    new CameraView({
+      layoutData: 'stretch',
+      camera
+    }).appendTo(contentView);
 }
 
