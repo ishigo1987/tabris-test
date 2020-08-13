e6f9fb5c22f1598337ad9e13a1b49155a7097148
ishigo1987 <stephnguets@yahoo.fr>
2020-08-13 10:53:37 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,29 +1,16 @@
 
-tabris.permission.requestAuthorization('storage')
+const{permission, contentView, Button} = require("tabris");
+permission.requestAuthorization('storage')
     .then(status => {
         if (status != 'granted'){
             console.error('No permissions!')
         }
         else {
-            let mediaPlayer = new MediaPlayer({url:'https://www.mymp3tracks.com/uploads/audio/[www.mymp3tracks.com]Locko-Memes-memes-choses.mp3'});
-            mediaPlayer.play()
-    
-            mediaPlayer.onReady(()=>{
-                console.log("test")
-            })
-            mediaPlayer.onError((e)=>{
-                console.log(e)
-            })
+            new Button({centerX:0, centerY:0, text:"Take an image",background:"blue"})
+            .onTap(()=>{
+              fs.openFile({type:"image/*"}).then((response)=>{
+                 console.log(response);
+              });
+            }).appendTo(contentView);
         }
-    })
-    .catch(err => console.error(err));
-
-
-    // const worker = new Worker("src/helpers/workerAudio.js");
-    //   worker.onmessage = (event) => {
-
-    //  };
-    // worker.onerror = (error) => {
-    //       console.log(`onerror: ${JSON.stringify(error)}`);
-    // };
-    // worker.postMessage("Nothing");
+    });
